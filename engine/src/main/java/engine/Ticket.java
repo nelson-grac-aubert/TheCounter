5ead48def35fc879ea25c235d3e9035aef354a84
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-09-23 13:14:10 +0200

    feat(engine): add tax breakdown fields to Ticket record

--- engine/src/main/java/engine/Ticket.java
+++ engine/src/main/java/engine/Ticket.java
@@ -2,6 +2,13 @@ package Engine.src.main.java.engine;
 
 import java.util.List;
 
-public record Ticket (List<TicketLine> lines, double total) {
+public record Ticket (
+    List<TicketLine> lines, 
+    double totalHt,
+    double vat5,
+    double vat20,
+    double totalTtc
+)
+{
     
 }
